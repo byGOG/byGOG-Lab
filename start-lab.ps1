ff0fb4b237c0305442ed91901c9a023fe1aa3a65
byGOG <Ond3rkor@gmail.com>
2025-07-23 03:26:10 +0300

    07

--- start-lab.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-    byGOG-Lab HTML sayfasını tarayıcıda açar.
-.DESCRIPTION
-    Bu betik, byGOG-Lab sayfasının en son sürümünü, dosyayı indirmeye
-    gerek kalmadan, doğrudan varsayılan web tarayıcısında açar.
-    Bu işlem için raw.githack.com servisi kullanılır.
-#>
-Start-Process -FilePath "https://raw.githack.com/byGOG/byGOG-Lab/main/byGOG-Lab.html"
