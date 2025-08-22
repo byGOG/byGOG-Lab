c632d533cece8dc80a4aa2f006da1c0ae621ce90
byGOG <Ond3rkor@gmail.com>
2025-08-22 23:39:05 +0300

    08

--- scripts/renderLinks.js
+++ scripts/renderLinks.js
@@ -178,4 +178,14 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.error(err);
   }
   setupSearch();
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 });
